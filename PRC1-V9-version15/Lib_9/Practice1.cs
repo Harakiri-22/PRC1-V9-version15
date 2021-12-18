7126b4d913bf22e774cbd58fa848ed7e6980120f
Mishin Dmitiriy <93830209+Harakiri-22@users.noreply.github.com>
2021-12-18 11:20:36 +0300

    Add files via upload

---
+++ PRC1-V9-version15/Lib_9/Practice1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_9
+{
+    public class Practice1
+    {
+        public static (int rez, string numb) AddingNumbers()
+        {
+            Random random = new Random();
+
+            int result = 0;
+
+            string str = string.Empty;
+
+            for (int i = 0; i < 13; i++)
+            {
+                int currentValue = random.Next(2, 14);
+                str += currentValue.ToString() + " ";
+
+                if (currentValue < 8)
+                {
+                    result += currentValue;
+                }
+            }
+            return (result, str);
+        }
+    }
+}
---
+++ PRC1-V9-version15/PRC1 V9/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib_9;
+
+namespace PRC1_V9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var result = Practice1.AddingNumbers();
+                number.Text = result.rez.ToString();
+                range.Text = result.numb;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Мишин Дмитрий ИСП-34", "Об авторе", MessageBoxButton.OK);
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
